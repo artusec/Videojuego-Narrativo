8f0d496e209261ceb185d2444f7dd9327dc2084f
Héctor Marcos Rabadán <hmarcos@ucm.es>
2020-03-19 14:01:15 +0100

    arreglo puzle formas
    mini arreglo en el puzle de reconocimiento de formas para que no se repitan llamadas a comenzar la vibracion

--- TFG_Unity/Assets/Scripts/EscapeRoom/MinijuegoForma.cs
+++ TFG_Unity/Assets/Scripts/EscapeRoom/MinijuegoForma.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class MinijuegoForma : MonoBehaviour
 {
+    //auxiliar para manejar que no se repitan llamadas a la vibracion
+    private bool lastCliked = false;
+
     private void OnMouseOver()
     {
         print("encima");
         if (Input.GetMouseButton(0))
         {
-            Vibration.SonarVibration(1, 1, 5, true);
-            //llamada a vibrar al maximo
-            print("clickado");
+            if (!lastCliked)
+            {
+                lastCliked = true;
+                Vibration.SonarVibration(1, 1, 0, true);
+                //llamada a vibrar al maximo
+                print("clickado");
+            }
         }
-        else Vibration.Cancel();
+        else
+        {
+            lastCliked = false;
+            Vibration.Cancel();
+        }
     }
 
     private void OnMouseExit()
     {
+        lastCliked = false;
         Vibration.Cancel();
     }
 }
