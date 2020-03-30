0a092c57838030dbd198cb53a61bd9be0e0463d6
Arturo Aguirre Calvo <44441475+artuyero@users.noreply.github.com>
2020-03-30 19:04:47 +0200

    juego_nuevo, cargar_partida, guardar_partida, borrar_usuario funcionando

--- BBDD/Servidor/borrar_usuario.php
+++ BBDD/Servidor/borrar_usuario.php
@@ -3,11 +3,11 @@ require_once __DIR__ . '/DB_data.php';
 require_once __DIR__ . '/User.php';
 
 $uri = $_SERVER["REQUEST_URI"];
-$id_user = $_POST["id_user"];
+$username = $_POST["username"];
 
 header("Content-type: text/html");
 
-if (User::borrar_usuario($id_user)){
+if (User::borrar_usuario("paco")){
 	print("Hecho!");
 	exit();
 }
--- BBDD/Servidor/nuevo_juego.php
+++ BBDD/Servidor/nuevo_juego.php
@@ -3,10 +3,14 @@ require_once __DIR__ . '/DB_data.php';
 require_once __DIR__ . '/Game.php';
 
 $uri = $_SERVER["REQUEST_URI"];
-$user = $_POST["username"];
+$username = $_POST["username"];
+
 header("Content-type: text/html");
-if (Game::inicia_nuevo_juego_individual($user)){
+
+if (Game::inicia_nuevo_juego_individual("paco")){
 	print("Hecho!");
 	exit();
 }
-print("Algo ha fallado");
+else{
+	print("Algo ha fallado creando el juego");
+}
