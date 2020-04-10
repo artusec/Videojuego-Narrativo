37571df0fdfae922d55c8ef8c5f899067408bf5b
Diego <dimart10@ucm.es>
2020-04-10 22:31:40 +0200

    incluido stt en Proyecto, singleton, llamadas en menus funcionando

---
+++ TFG_Unity/Assets/Scripts/ScreenReader/Actables/FillWithSTT.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillWithSTT : Actable
+{
+    public Text target = null;
+
+    public override void Act()
+    {
+        STT.instance.Listen(target);
+    }
+}
