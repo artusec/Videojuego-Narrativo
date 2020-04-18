13afec53ac35528c4014f9c02e0e0e033ff1c0bd
Diego <dimart10@ucm.es>
2020-04-18 22:44:15 +0200

    Boton web en mainmenu

---
+++ TFG_Unity/Assets/Scripts/ScreenReader/Actables/LoadWeb.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadWeb : Actable
+{
+    public string url = "http://laslomasiii.serveftp.net:4398/inicio.php";
+    public AudioClip avisoCarga;
+
+
+    public override void Act()
+    {
+        if (element.getState() == objectState.DEFAULT)
+        {
+            SRManager.instance.playTTS(avisoCarga);
+            SRManager.instance.deactivate(avisoCarga.length);
+            Invoke("openWeb", avisoCarga.length);
+        }
+    }
+
+    void openWeb()
+    {
+        Application.OpenURL(url);
+    }
+
+}
