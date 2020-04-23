870565c7ff7233d03426012ba2d940ff9dc12e48
Arturo <artuagui@ucm.es>
2020-04-23 18:42:15 +0200

    Scripts separados

--- Web/Servidor/web/Logout.php
+++ Web/Servidor/web/Logout.php
@@ -1,12 +1,5 @@
 <?php
 	require_once '../DB_data.php';
-
-	unset($_SESSION["login"]);
-	unset($_SESSION["profesor"]);
-	unset($_SESSION["administrador"]);
-	unset($_SESSION['id']);
-	unset($_SESSION['nombre']);
-
 	session_destroy();
 	require_once __DIR__.'/inicio.php';
 ?>
