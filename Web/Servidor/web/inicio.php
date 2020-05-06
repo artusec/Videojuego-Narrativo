ed016a7ba35d36ab970ab733bf6996b346c16dbe
Fer <ferlobo1998@gmail.com>
2020-05-07 01:10:32 +0200

    Cambios en etiquestas para mas accesibilidad

--- Web/Servidor/web/inicio.php
+++ Web/Servidor/web/inicio.php
@@ -7,7 +7,10 @@
     
     <head>
        
-    <meta charset="UTF-8" content="width=device-width, initial-scale=1.0" name="viewport">   
+    <meta charset="UTF-8">
+    <meta name="description" content="ASHED MEMORIES - JUEGO ACCESIBLE" >
+    <meta name="keywords" content="ACCESIBILIDAD, JUEGO, ASHED, MEMORIES">
+    <meta  name="viewport" content="width=device-width, initial-scale=1.0">   
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
@@ -47,7 +50,7 @@
     require_once './generic/header.php';
 ?>
 
-<section id="cabecera">
+<section aria-label="Cabecera">
         <div class="row logo">
             <div class="col-lg-4 col-md-12">
                     <div class="Name">
@@ -74,7 +77,7 @@
 
     <?php if(!isset($_COOKIE["cookie"])) { ?>
 
-    <div class="modal fade in" id="myModal" role="dialog" aria-describedby="modal-dialog" aria-labelledby="titulo-modal">
+    <div class="modal fade in" id="myModal" role="dialog" aria-labelledby="titulo-modal">
         <div class="modal-dialog modal-md modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header d-flex justify-content-center align-items-center">
@@ -85,10 +88,10 @@
                 <div class="row d-flex justify-content-center align-items-center">
                     <p class="pt-3 pr-2 ">Utilizamos cookies para mejorar la experiencia en la web.</p>
                     <br aria-hidden="true"> <br aria-hidden="true"> <br aria-hidden="true"> <br aria-hidden="true">
-                    <a type="button" class="btn btn-cookies " aria-label="Leer mas sobre cookies" href="./Cookies.html">Leer mas </a>
+                    <a role="button" class="btn btn-cookies " aria-label="Leer mas sobre cookies" href="./Cookies.html">Leer mas </a>
                     &nbsp
                     &nbsp
-                    <a type="button" class="btn btn-cookies" data-dismiss="modal" aria-label="Vale, gracias" href="" onclick='activarCookie();'>Ok, gracias</a>
+                    <a role="button" class="btn btn-cookies" data-dismiss="modal" aria-label="Vale, gracias" href="" onclick='activarCookie();'>Ok, gracias</a>
                     <br aria-hidden="true"> <br aria-hidden="true"> <br aria-hidden="true"> <br aria-hidden="true">
                 </div>
             </div>
@@ -99,10 +102,8 @@
 
     <?php } ?>
 
-    <section aria-labely="Introducción">
+<section aria-label="Introducción">
     <div class="container-fluid">
-
-      
     <br aria-hidden="true">
         <?php
             if (isset($_SESSION['login']) && $_SESSION['login'] === true){
@@ -111,7 +112,7 @@
         <br aria-hidden="true"> 
         <?php
             }else{  ?>
-                <h1 class="text-center" id='1'>¡TE DAMOS LA BIENVENIDA A ASHED MEMORIES!</p></h1>
+                <h1 class="text-center" id='1'>¡TE DAMOS LA BIENVENIDA A ASHED MEMORIES!</h1>
                 <br aria-hidden="true"> 
         <?php    }
         ?>
@@ -123,7 +124,7 @@
 
         <br aria-hidden="true">
         <div class="col text-center" id='4'>
-            <a  type="button" class="btn btn-danger btn-lg" id="descarga" aria-label="Descarga Ashed Memories"
+            <a role="button"  class="btn btn-danger btn-lg" id="descarga" aria-label="Descarga Ashed Memories"
              download="AshedMemories.apk" href="./apk/AshedMemories_1.0.apk" >¡Descarga ya!<img src="./imagenes/download.png" alt=""></a>
         </div>
         <br aria-hidden="true">
@@ -137,7 +138,7 @@
         <div class="col-md-4 ">    
             <div class="minijuegosOut ">
                     <div class="card cartas-prop">
-                        <a href="./accesibilidad.php"><img src="./imagenes/accesibility.png"  class="card-img-top" alt="" aria-label="Leer mas sobre accesibilidad"></a>
+                        <a href="./accesibilidad.php"  aria-hidden=true><img src="./imagenes/accesibility.png"  class="card-img-top" alt="" aria-label="Leer mas sobre accesibilidad"></a>
                         <div class="card-body">
                             <h2 class="card-text">ACCESIBILIDAD</h2>
                             <p class="card-text">El juego es accesible de forma nativa ya que imita los movimientos de un lector de pantalla.</p>
@@ -150,7 +151,7 @@
         <div class="col-md-4">    
             <div class="minijuegosOut">
                     <div class="card cartas-prop">
-                        <a href="./nube.php"> <img src="./imagenes/nube.png"  class="card-img-top "alt="" aria-label="Leer mas sobre la nube"> </a>
+                        <a href="./nube.php" aria-hidden=true> <img src="./imagenes/nube.png"   class="card-img-top" alt="" aria-label="Leer mas sobre la nube"> </a>
                         <div class="card-body">
                             <h2 class="card-text">NUBE</h2>
                             <p class="card-text">El juego tiene implementado dos sistemas de guardado de tus datos y partidas. Local y en la nube.</p>
@@ -169,109 +170,112 @@
     <div class="container-fluid">
         <br aria-hidden="true">
         <br aria-hidden="true">
-        <p><h1 class=" text-center fade-in" id="minijuegos-title">¡Conoce los minijuegos!</h1></p>
+        <h1 class=" text-center fade-in" id="minijuegos-title">¡Conoce los minijuegos!</h1>
         <p class="text-center"><img src="./imagenes/jigsaw.png" alt=""></p>
         <br aria-hidden="true">
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="minijuegosOut">
-                            <div class="card cartas-minijuegos">
-                                <a href="./ganzua.php"><img class="card-img-top" src="./imagenes/safe_box.png" alt="" aria-label="Leer mas sobre el minijuego ganzua"></a>
-                                <div class="card-body">
-                                  <h2 class="card-text">GANZUA</h2>
-                                  <p class="card-text">Hacer círculos con el dedo sobre la pantalla hasta oír el desbloqueo de la caja fuerte.</p>
-                                  <br aria-hidden="true">
-                                  <a href="./ganzua.php" class="leer-mas" aria-label="Leer mas sobre el minijuego ganzua">Leer mas</a>
-                                </div>
-                            </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">   
-                    <div class="minijuegosOut">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="minijuegosOut">
                         <div class="card cartas-minijuegos">
-                            <a href="./voces.php"><img class="card-img-top" src="./imagenes/earphone.png" alt="" aria-label="Leer mas sobre el minijuego voces a lo lejos"></a>
+                            <a href="./ganzua.php" aria-hidden=true><img class="card-img-top"  src="./imagenes/safe_box.png" alt="" aria-label="Leer mas sobre el minijuego ganzua"></a>
                             <div class="card-body">
+                                <h2 class="card-text">GANZUA</h2>
+                                <p class="card-text">Hacer círculos con el dedo sobre la pantalla hasta oír el desbloqueo de la caja fuerte.</p>
+                                <br aria-hidden="true">
+                                <a href="./ganzua.php" class="leer-mas" aria-label="Leer mas sobre el minijuego ganzua">Leer mas</a>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="col-sm-4">   
+                <div class="minijuegosOut">
+                    <div class="card cartas-minijuegos">
+                        <a href="./voces.php" aria-hidden=true><img class="card-img-top"  src="./imagenes/earphone.png" alt="" aria-label="Leer mas sobre el minijuego voces a lo lejos"></a>
+                        <div class="card-body">
                             <h2 class="card-text">VOCES A LO LEJOS</h2>
                             <p class="card-text">Mover el dedo por la pantalla siguiendo unas voces que podrás oír en alguna dirección.</p>
                             <br aria-hidden="true">
-                              <a href="./voces.php" class="leer-mas" aria-label="Leer mas sobre el minijuego voces a lo lejos">Leer mas</a>
-                            </div>
+                            <a href="./voces.php" class="leer-mas" aria-label="Leer mas sobre el minijuego voces a lo lejos">Leer mas</a>
                         </div>
-                </div>
-                </div>
-                <div class="col-sm-4">    
-                    <div class="minijuegosOut">
-                            <div class="card cartas-minijuegos">
-                                <a href="./formas.php"><img class="card-img-top" src="./imagenes/geometry.png" alt="" aria-label="Leer mas sobre el minijuego formas"></a>
-                                <div class="card-body">
-                                <h2 class="card-text">FORMAS</h2>
-                                <p class="card-text">Pasar el dedo por la pantalla y detectar vibraciones para adivinar la forma del objeto escondido.</p>
-                                <a href="./formas.php" class="leer-mas" aria-label="Leer mas sobre el minijuego formas">Leer mas</a>
-                                </div>
-                            </div>
                     </div>
                 </div>
             </div>
-            <br aria-hidden="true">
-            <br aria-hidden="true">
-            <br aria-hidden="true">
-            <br aria-hidden="true">
-            <div class="container-fluid">
-            <br aria-hidden="true">
-                <div class="row">
-                    <div class="col-sm-4"></div>
-                    <div class="col-sm-4">
-                        <div class="ojo">
-                            <div class="shut">
-                                <span></span>
-                            </div>
-                            <div class="ball">
-                                <div class='eye'>
-                                    <div class='luz'></div>
-                                </div>
-                            </div>
+            <div class="col-sm-4">    
+                <div class="minijuegosOut">
+                    <div class="card cartas-minijuegos">
+                        <a href="./formas.php" aria-hidden=true><img class="card-img-top"  src="./imagenes/geometry.png" alt="" aria-label="Leer mas sobre el minijuego formas"></a>
+                        <div class="card-body">
+                            <h2 class="card-text">FORMAS</h2>
+                            <p class="card-text">Pasar el dedo por la pantalla y detectar vibraciones para adivinar la forma del objeto escondido.</p>
+                            <a href="./formas.php" class="leer-mas" aria-label="Leer mas sobre el minijuego formas">Leer mas</a>
                         </div>
-                    <div class="col-sm-4"></div>
-                </div>
-                <br aria-hidden="true">
-                <br aria-hidden="true">
-                <br aria-hidden="true">
-                <br aria-hidden="true">
-            <br aria-hidden="true">
-            </div>
-            <br aria-hidden="true">
-            <br aria-hidden="true">
-            <br aria-hidden="true">
-            <br aria-hidden="true">
-            <br aria-hidden="true">
-            <div class="row">
-            <div class="col-sm-2"></div>
-                <div class="col-sm-4">    
-                    <div class="minijuegosOut">
-                            <div class="card cartas-minijuegos">
-                                <a href="./puntos.php"><img class="card-img-top" src="./imagenes/diana.png" alt="" aria-label="Leer mas sobre el minijuego puntos calientes"></a>
-                                <div class="card-body">
-                                <p class="card-text"><h2>PUNTOS CALIENTES</h2></p>
-                                <p class="card-text">Pasar el dedo por la pantalla y detectar zonas de vibración más intensa para encontrar el punto exacto.</p>
-                                <a href="./puntos.php" class="leer-mas" aria-label="Leer mas sobre el minijuego puntos calientes">Leer mas</a>
-                                </div>
-                            </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">    
-                    <div class="minijuegosOut">
-                            <div class="card cartas-minijuegos">
-                                <a href="./simon.php"><img class="card-img-top" src="./imagenes/simondice.png" alt="" aria-label="Leer mas sobre el minijuego formas"></a>
-                                <div class="card-body">
-                                <p class="card-text"><h2>SIMON SAYS</h2></p>
-                                <p class="card-text">Pasar el dedo por la pantalla y jugar al "Simon says" de toda la vida que hemos jugado todos de pequeños.</p>
-                                <a href="./simon.php" class="leer-mas" aria-label="Leer mas sobre el minijuego formas">Leer mas</a>
-                                </div>
-                            </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+        <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <div class="ojo">
+                    <div class="shut">
+                        <span></span>
+                    </div>
+                    <div class="ball">
+                        <div class='eye'>
+                            <div class='luz'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4"></div>
+            <br aria-hidden="true">
+            <br aria-hidden="true">
+            <br aria-hidden="true">
+            <br aria-hidden="true">
+        </div>
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+
+        <div class="row">
             <div class="col-sm-2"></div>
+            <div class="col-sm-4">    
+                <div class="minijuegosOut">
+                    <div class="card cartas-minijuegos">
+                        <a href="./puntos.php" aria-hidden=true><img class="card-img-top"  src="./imagenes/diana.png" alt="" aria-label="Leer mas sobre el minijuego puntos calientes"></a>
+                        <div class="card-body">
+                            <h2 class="card-text">PUNTOS CALIENTES</h2>
+                            <p class="card-text">Pasar el dedo por la pantalla y detectar zonas de vibración más intensa para encontrar el punto exacto.</p>
+                            <a href="./puntos.php" class="leer-mas" aria-label="Leer mas sobre el minijuego puntos calientes">Leer mas</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">    
+                <div class="minijuegosOut">
+                    <div class="card cartas-minijuegos">
+                        <a href="./simon.php"  aria-hidden=true><img class="card-img-top" src="./imagenes/simondice.png" alt="" aria-label="Leer mas sobre el minijuego formas"></a>
+                        <div class="card-body">
+                            <h2 class="card-text">SIMON SAYS</h2>
+                            <p class="card-text">Pasar el dedo por la pantalla y jugar al "Simon says" de toda la vida que hemos jugado todos de pequeños.</p>
+                            <a href="./simon.php" class="leer-mas" aria-label="Leer mas sobre el minijuego formas">Leer mas</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2"></div>
+        </div>
+        <br aria-hidden="true">
+        <br aria-hidden="true">
+        <br aria-hidden="true">
         <br aria-hidden="true">
         <br aria-hidden="true">
         <br aria-hidden="true">
@@ -287,7 +291,6 @@
 <a href="#"  aria-label="Volver al principio de la página" class="back-to-top"><img id="top-icon" src="./imagenes/top.png" alt=""></a>
 
 
-</body>
 
 <script>
 
@@ -304,24 +307,24 @@ function activarCookie() {
 
 
 
- /*BOTON DE IR ARRIBA*/
- $(window).scroll(function() {
+/*BOTON DE IR ARRIBA*/
+$(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
+    $('.back-to-top').fadeIn('slow');
     } else {
-      $('.back-to-top').fadeOut('slow');
+    $('.back-to-top').fadeOut('slow');
     }
-  });
+});
 
-  $('.back-to-top').click(function() {
+$('.back-to-top').click(function() {
     $('html, body').animate({
-      scrollTop: 0
+    scrollTop: 0
     }, 1000, 'linear');
     return false;
-  });
+});
 
 
-  /*ANIMACION DEL OJO*/
+/*ANIMACION DEL OJO*/
 $("body").mousemove(function(event) {
     var eye = $(".eye");
     var eyex = (eye.offset().left) + (eye.width() / 2);
@@ -347,5 +350,6 @@ $("body").mousemove(function(event) {
         
 
 </script>
+</body>
 
 </html>
